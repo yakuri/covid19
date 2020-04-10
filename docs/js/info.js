6a7c880f98b326a98d3fa4a9da45e13cce0a728e
Yasutake Kurita <62694113+yakuri@users.noreply.github.com>
2020-04-11 00:42:53 +0900

    修正を反映

--- docs/js/info.js
+++ docs/js/info.js
@@ -1,25 +1,58 @@
-var chartData = {};
-chartData.type = 'horizontalBar';
-chartData.data = {
+var chartDataKansen = {};
+chartDataKansen.type = 'bar';
+chartDataKansen.data = {
   labels: [{}],
   datasets: [
     {
       label: '感染数',
       data: [{}],
-      backgroundColor: "rgba(219,39,91,0.5)"
+      backgroundColor: "rgba(219,39,91,0.5)",
     },
   ]
 };
-chartData.options = {
+chartDataKansen.options = {
   title: {
     display: true,
-    text: '都道府県別 感染数'
+    text: '都道府県別 感染数（上位10都道府県）'
   },
   scales: {
     xAxes: [{
         ticks: {
         }
-    }]
+    },],
+    yAxes: [{
+        ticks: {
+        }
+    },]
+  },
+};
+
+var chartDataShisha = {};
+chartDataShisha.type = 'bar';
+chartDataShisha.data = {
+  labels: [{}],
+  datasets: [
+    {
+      label: '死者数',
+      data: [{}],
+      backgroundColor: "rgba(100,39,91,0.5)",
+    },
+  ]
+};
+chartDataShisha.options = {
+  title: {
+    display: true,
+    text: '都道府県別 死者数（上位10都道府県）'
+  },
+  scales: {
+    xAxes: [{
+        ticks: {
+        }
+    },],
+    yAxes: [{
+        ticks: {
+        }
+    },]
   },
 };
 
@@ -32,6 +65,13 @@ $.ajax({
     $("#output").append("<tr><th>都道府県名</th><th>感染数</th><th>死者数</th><th>感染数における死者数の割合</th></tr>");
     var totalcases = 0;
     var totaldeaths = 0;
+
+    // 感染数でソート
+    data1.sort(function(a,b){
+      if(a.cases > b.cases) return -1;
+      if(a.cases < b.cases) return 1;
+      return 0;
+    });
     for(var i in data1){
         var parcent = 0;
         if(data1[i].cases != 0) {
@@ -47,8 +87,11 @@ $.ajax({
           totaldeaths = totaldeaths + data1[i].deaths;
 
           // グラフ表示用
-          chartData.data.labels[i] = data1[i].name_ja;
-          chartData.data.datasets[0].data[i] = data1[i].cases;
+          if (i < 10) {
+              chartDataKansen.data.labels[i] = data1[i].name_ja;
+              chartDataKansen.data.datasets[0].data[i] = data1[i].cases;
+              //chartDataKansen.data.datasets[1].data[i] = data1[i].deaths;
+          }
     }
     $("#output").append("<tr><td>計</td><td>" + totalcases
       + "名</td><td>" + totaldeaths
@@ -56,9 +99,25 @@ $.ajax({
       + "%</td></tr>");
     var data2 = JSON.stringify(data1);
 
+    // 死者数でソート
+    data1.sort(function(a,b){
+      if(a.deaths > b.deaths) return -1;
+      if(a.deaths < b.deaths) return 1;
+      return 0;
+    });
+    for(var i in data1){
+      // グラフ表示用
+      if (i < 10) {
+          chartDataShisha.data.labels[i] = data1[i].name_ja;
+          chartDataShisha.data.datasets[0].data[i] = data1[i].deaths;
+      }
+    }
+
     // グラフ表示用
-    var ctx = document.getElementById('chart');
-    var chart = new Chart(ctx, chartData);
+    var ctx = document.getElementById('chartKansen');
+    var chart = new Chart(ctx, chartDataKansen);
+    ctx = document.getElementById('chartShisha');
+    var chart = new Chart(ctx, chartDataShisha);
 
     console.log(data2);
   }).fail(function(jqXHR, textStatus, errorThrown ) {
